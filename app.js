ed8b74d8d6fa86f6ab799941e76f6756e7bb53aa
007anish <anish159@hotmail.com>
2021-05-03 11:45:43 +0100

    step-progress-bar added

--- app.js
+++ app.js
@@ -16,3 +16,16 @@ const requires = [
 window.app = angular.module('app', requires);
 
 angular.bootstrap(document.getElementById('app'), ['app']);
+var myApp = window.app;
+myApp.controller("myController", function($scope) {
+  $scope.current_progress = 3; // set the current working progress
+  $scope.steps = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  $scope.activeQuestNode = function(index, max){
+		if(index == max - 1) // - 1 to adjust the array index
+			return "current_active";
+		if(index < max - 1)
+			return "active"
+		return "";
+	};
+});
